1ead4970e43a986f6849f89ded031f60408bfd83
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-07-14 02:15:08 +0530

    add

--- my-app/src/Components/Home.jsx
+++ my-app/src/Components/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import styles from "./Home.module.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <nav className={styles.navbar}>
+
+            <div className={styles.navDiv}>
+          
+            
+                    <h2>Home</h2>
+                    <h2>About</h2>
+                    <h2>Todo</h2>
+                    <h2>Shopping Bag</h2>
+                    <h2>Contact</h2>
+            
+            </div>
+
+        </nav>
+    </div>
   )
 }
 
