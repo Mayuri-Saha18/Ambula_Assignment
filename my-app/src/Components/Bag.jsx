9d58148da6f3469f9bbb1692ea4670af53ea061c
Mayuri-Saha18 <105915693+Mayuri-Saha18@users.noreply.github.com>
2023-07-14 19:46:42 +0530

    shopping data

--- my-app/src/Components/Bag.jsx
+++ my-app/src/Components/Bag.jsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+import { Link } from 'react-router-dom';
+import styles from './Bag.module.css';
 
 const Bag = () => {
+
+
+    
+    const [products, setProducts]=useState([]);
+
+        useEffect(()=>{
+
+            const fetchProducts=async ()=>{
+
+            const response=await axios.get(`https://dummyjson.com/products/category/smartphones`);
+
+            setProducts(response.data.products);
+
+            console.log(response.data)
+            console.log(response.data.products)
+
+            };
+
+
+            fetchProducts();
+
+        },[]);
+
+
+
+  const handleWishlist=(product)=>{
+
+    const wishlist=JSON.parse(localStorage.getItem('wishlist')) || [];
+
+    if (!wishlist.some((item)=>item.id===product.id)){
+
+      wishlist.push(product);
+
+      localStorage.setItem('wishlist', JSON.stringify(wishlist));
+
+      alert("Item added to cart")
+
+    }
+    
+    else{
+
+      alert("Item already added to cart")
+    }
+    
+  };
+
+
   return (
-    <div>Bag</div>
+    <div>
+
+            
+
+                {products.length?(
+
+                    <div className={styles.container}>
+
+
+                            {products.map((product)=>(
+
+                                <div key={product.id}>
+
+                                <div className={styles.card}>
+
+                                    <img src={product.thumbnail} className={styles.imgcard} alt={product.title} />
+                                    <div className={styles.cardbody}>
+
+                                            <h1>Name: {product.title}</h1>
+                                            <h4>Description: {product.description}</h4>
+                                            <p>Price: {product.price}</p>
+
+
+                                            <Link to="#" onClick={()=>handleWishlist(product)} className="btn btn-outline-danger"><FontAwesomeIcon icon={faHeart}/></Link>
+                                    </div>
+
+
+                                </div>
+
+                                </div>
+                            ))}
+
+
+                    </div>
+
+                ) : (
+
+                    <div>Loading...</div>
+
+                )}
+    </div>
   )
 }
 
--- my-app/src/Components/Cart.jsx
+++ my-app/src/Components/Cart.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import styles from './cart.module.css';
 
 const Cart = () => {
+
+    const [wishlistItems, setWishlistItems]=useState([]);
+
+  
+        useEffect(()=>{
+
+            const items=JSON.parse(localStorage.getItem("wishlist") || "[]");
+
+            setWishlistItems(items);
+
+        }, []);
+        // useeffect works after initial render and show all the items from the localstorage as initial we have an empty array and updated the setwishlist
+
+  
+    const handleRemoveItem=(id)=>{
+
+        const updatedItems=wishlistItems.filter((item)=>item.id!==id);
+
+
+        setWishlistItems(updatedItems);
+
+        localStorage.setItem("wishlist", JSON.stringify(updatedItems));
+    };
+    // the remove item remove the item if the filter data item matches with the given id then it will delete the product and updated the localstorage
+
+
+    // Calculate the total number of items and the subtotal
+  const totalItems=wishlistItems.length;
+  const subtotal=wishlistItems.reduce((total, item) => total + item.price, 0);
+
+
   return (
-    <div>Cart</div>
+
+
+    <div>
+
+
+            <div className={styles.summary}>
+
+                    <h3>Total Items: {totalItems}</h3>
+                    <h3>Subtotal: {subtotal.toFixed(2)}</h3>
+
+            </div>
+
+            {wishlistItems.length>0 ? (
+
+                    <div className={styles.productgrid}>
+
+
+                        {wishlistItems.map((item)=>(
+
+                            <div key={item.id} className={styles.productcard}>
+
+
+                                    <img src={item.thumbnail} alt={item.title}/>
+
+
+                                    <h3>Title: {item.title}</h3>
+
+                                    <p>Description: {item.description}</p>
+
+                                    <p>Price: {item.price}</p>
+
+                                    <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
+
+                            </div>
+
+                        ))}
+
+                            
+                    </div>
+                ) : (
+
+
+                    <p>You have no items in your wishlist.</p>
+
+                )}
+    </div>
   )
 }
 
